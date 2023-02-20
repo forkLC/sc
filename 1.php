838d38d59d2e17d6dbdc71b3c8e484431c73e83e
Lc <123912910+forkLC@users.noreply.github.com>
2023-02-20 11:57:00 +0700

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,3 +1,5 @@
 <?php
 echo "SC SUDAH MAMPUS, MAU PESAN YANG BARU? HUBUNGI TELEGRAM ADMIN @lc_dev dan @lc_ScWingoReal .  Jangan lupa perbarui dulu SC ini pilih menu nomor 6 ..\n\n\n";
-system("exit");
+goto close;
+
+close:
